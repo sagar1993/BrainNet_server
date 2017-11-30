5089f0661b20334191aa134156de99c9960381b7
shpatni <shpatni@asu.edu>
2017-11-29 19:38:35 -0700

    message

--- service/models.py
+++ service/models.py
@@ -5,4 +5,4 @@ from django.db import models
 
 class UserData(models.Model):
     user_name = models.CharField('user_name', max_length=200,blank=True, null=True)
-    brain_signal = models.CharField('brain_signal', blank=True, null=True)
+    brain_signal = models.CharField('brain_signal', max_length=5000, blank=True, null=True)
